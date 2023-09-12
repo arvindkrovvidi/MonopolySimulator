86b665a4e7163b2ec5a48f6814cbedf41c3fd589
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-09-12 22:53:51 +0530

    Added type hints and doc strings for functions in Player class

--- Player.py
+++ Player.py
@@ -1,4 +1,6 @@
 import random
+
+import Tile
 from PropertyIterators import PropertyList
 from Board import max_tile_no, go_cash
 from Property_data import property_tracker
@@ -23,22 +25,34 @@ class Player:
     def __str__(self):
         return self.name
 
-    def buy_property(self, asset):
+    def buy_property(self, asset: Tile.Property) -> None:
+        """
+        Buy property that the player lands on.
+        :param asset: Property
+        """
         if asset.cost <= self.cash:
             self.cash -= asset.cost
             self.player_portfolio.append(asset)
             property_tracker.update_dict({asset: self})
 
-    def throw_dice(self):
+    def throw_dice(self) -> int:
+        """
+        Simulates a two dice throw by generating a random number between 2 and 12.
+        :return: Random integer between 2 and 12
+        """
         return random.randint(2, 12)
 
-    def move(self, throw):
+    def move(self, throw: int) -> None:
+        """
+        Move the player to a tile based on the dice throw
+        :param throw: The dice throw
+        """
         self.tile_no += throw
         if self.tile_no > max_tile_no:
             self.tile_no -= max_tile_no
             self.cash += go_cash
 
-    def pay_rent(self, player, rent):
+    def pay_rent(self, player: Player, rent: int) -> None:
         self.cash -= rent
         player.cash += rent
 
