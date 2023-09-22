2becd1dee658fb1f5441a9a405e0da36cc61f024
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-09-22 18:22:19 +0530

    REFACTOR: Remove commented code.

--- ChanceTile.py
+++ ChanceTile.py
@@ -7,10 +7,4 @@ class ChanceTile(SpecialTiles):
         self._description = description
         Tile.__init__(self, tile_no, name)
 
-    # def execute(self, player):
-    #     if self._description == "Advance to Boardwalk":
-    #         player.move_to(boardwalk, collect_go_cash_flag=False)
-    #     elif self._description == "Advance to Go (Collect $200)":
-    #         player.move_to(go)
-    #     elif self._description == "Advance to Illinois Avenue. If you pass Go, collect $200":
-    #         player.move_to(illinois_avenue)
+
