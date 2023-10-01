3cb07b893cbc338c9cf3a310694716eec2dce32e
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-01 21:41:55 +0530

    FEAT: Add community chest cards

--- CommunityChestTile.py
+++ CommunityChestTile.py
@@ -1,6 +1,6 @@
 from SpecialTiles import SpecialTiles
 from Tile import Tile
-
+from special_tiles_data import go
 
 class CommunityChestTile(SpecialTiles):
     def __init__(self, name, tile_no, description=None):
@@ -10,34 +10,35 @@ class CommunityChestTile(SpecialTiles):
     @staticmethod
     def execute(player, _card_no):
         if _card_no == 1:
-            pass
+            player.move_to(go)
+            player.bank_transaction(200)
         if _card_no == 2:
-            pass
+            player.bank_transaction(200)
         if _card_no == 3:
-            pass
+            player.bank_transaction(-50)
         if _card_no == 4:
-            pass
+            player.bank_transaction(50)
         if _card_no == 5:
             pass
         if _card_no == 6:
             pass
         if _card_no == 7:
-            pass
+            player.bank_transaction(100)
         if _card_no == 8:
-            pass
+            player.bank_transaction(20)
         if _card_no == 9:
             pass
         if _card_no == 10:
-            pass
+            player.bank_transaction(100)
         if _card_no == 11:
-            pass
+            player.bank_transaction(-100)
         if _card_no == 12:
-            pass
+            player.bank_transaction(-50)
         if _card_no == 13:
-            pass
+            player.bank_transaction(25)
         if _card_no == 14:
             pass
         if _card_no == 15:
-            pass
+            player.bank_transaction(10)
         if _card_no == 16:
-            pass
+            player.bank_transaction(100)
