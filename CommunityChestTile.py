386278dd76c29f9146b6f60e5111e6de9c4c44f8
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-01 21:30:49 +0530

    FEAT: Add if else block for all community chest cards

--- CommunityChestTile.py
+++ CommunityChestTile.py
@@ -8,5 +8,36 @@ class CommunityChestTile(SpecialTiles):
         Tile.__init__(self, tile_no, name)
 
     @staticmethod
-    def execute(player, card_no):
-        pass
+    def execute(player, _card_no):
+        if _card_no == 1:
+            pass
+        if _card_no == 2:
+            pass
+        if _card_no == 3:
+            pass
+        if _card_no == 4:
+            pass
+        if _card_no == 5:
+            pass
+        if _card_no == 6:
+            pass
+        if _card_no == 7:
+            pass
+        if _card_no == 8:
+            pass
+        if _card_no == 9:
+            pass
+        if _card_no == 10:
+            pass
+        if _card_no == 11:
+            pass
+        if _card_no == 12:
+            pass
+        if _card_no == 13:
+            pass
+        if _card_no == 14:
+            pass
+        if _card_no == 15:
+            pass
+        if _card_no == 16:
+            pass
