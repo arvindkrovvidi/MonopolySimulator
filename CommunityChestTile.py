3204697cf220925257215cf11c4629c931deb5b3
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-02 18:15:00 +0530

    FEAT: Add function for community chest card 9

--- CommunityChestTile.py
+++ CommunityChestTile.py
@@ -1,6 +1,8 @@
+from Player_data import all_players_list
 from SpecialTiles import SpecialTiles
 from Tile import Tile
 from special_tiles_data import go
+
 
 class CommunityChestTile(SpecialTiles):
     def __init__(self, name, tile_no, description=None):
@@ -27,7 +29,7 @@ class CommunityChestTile(SpecialTiles):
         if _card_no == 8:
             player.bank_transaction(20)
         if _card_no == 9:
-            pass
+            execute_chest_9(player, all_players_list)
         if _card_no == 10:
             player.bank_transaction(100)
         if _card_no == 11:
@@ -42,3 +44,7 @@ class CommunityChestTile(SpecialTiles):
             player.bank_transaction(10)
         if _card_no == 16:
             player.bank_transaction(100)
+
+def execute_chest_9(player, players):
+    for each_player in players:
+        player.player_transaction(each_player, 10)
