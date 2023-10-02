701e08cb7a934216fb6f4cdcc8f2f8cb243d82d5
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-02 18:55:25 +0530

    DOCS: Added docs for TileIterators.py functions

--- TileIterators.py
+++ TileIterators.py
@@ -6,7 +6,12 @@ class TileList:
         self.data = data
         self.index = 0
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: int):
+        """
+        Get a tile from the list
+        :param key: The tile number
+        :return: Requested tile
+        """
         if isinstance(key, int):
             if key < 0:
                 raise IndexError("Tile number does not exist")
@@ -20,6 +25,10 @@ class TileList:
         return self
 
     def __next__(self):
+        """
+        Get the next tile in the list
+        :return: next tile in the list
+        """
         if self.index < len(self.data):
             result = self.data[self.index]
             self.index += 1
@@ -28,38 +37,66 @@ class TileList:
             raise StopIteration
 
     def append(self, asset):
+        """
+        Add a tile to the list
+        :param asset: The tile to be added to the list
+        """
         self.data.append(asset)
 
     def __contains__(self, item):
+        """
+        Check if a tile exists in the list
+        :param item:
+        :return:
+        """
         for asset in self.data:
             if asset.tile_no == item.tile_no:
                 return item
 
     def __add__(self, other):
+        """
+        Overload the '+' operator to concatenate two tile lists
+        :param other: The tile list to be concatenated to the first tile list
+        :return: The concatenated tile list
+        """
         for each in other:
             self.append(each)
         return self
 
     def get(self, value):
+        """
+        Get the tile being requested based on tile number
+        :param value: The tile number
+        :return: The requested tile
+        """
         for asset in self.data:
             if asset == value:
                 return asset
 
 class TileDict:
     """
-    A dictionary of properties and their owners. Data is a dictionary. assets are list of properties. Values are list of players.
+    A dictionary of properties and their owners. Data is a dictionary. Assets are list of properties. Values are list of players.
     """
     def __init__(self, data):
         self.data = data
         self.index = 0
 
     def __getitem__(self, asset):
+        """
+        Get the owner of the tile
+        :param asset: The tile
+        :return: The owner of the tile
+        """
         if asset in self.data:
             return self.data.get(asset)
         else:
             raise KeyError(f"{asset} does not exist in the dictionary")
 
     def __next__(self):
+        """
+        Get the next tile-person pair in the dictionary
+        :return: Next tile-person pair
+        """
         if self.index < len(list(self.data.keys())):
             result = self[list(self.data.keys())[self.index]]
             self.index += 1
@@ -71,13 +108,26 @@ class TileDict:
         return self
 
     def __contains__(self, asset):
+        """
+        Check if tile exists in the dictionary
+        :param asset: Tile
+        :return:
+        """
         for each in list(self.data.keys()):
             if each.tile_no == asset.tile_no:
                 return each
 
     def update_dict(self, dictionary):
+        """
+        Add a Tile-Person pair to the dictionary
+        :param dictionary:
+        """
         self.data.update(dictionary)
         list(self.data.keys()).append(list(dictionary.keys())[0])
 
     def keys(self):
+        """
+        Return the list of tiles in the dictionary
+        :return: List of tiles
+        """
         return self.data.keys()
