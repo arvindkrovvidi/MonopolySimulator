07f979b27b4c8b293157786eca69797e53935f93
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-03 16:04:21 +0530

    FEAT: Turned Rent into a property to add hotel and houses

--- Property.py
+++ Property.py
@@ -6,8 +6,11 @@ class Property(Tile):
     def __init__(self, tile_no, name, cost, rent):
         Tile.__init__(self, tile_no, name)
         self.cost = cost
-        self.rent = rent
+        self._rent = rent
         self._owner = None
+        self._color_set = True
+        self._houses = 0
+        self._hotel = False
 
     @property
     def owner(self):
@@ -16,6 +19,17 @@ class Property(Tile):
     @owner.setter
     def owner(self, value):
         self._owner = value
+
+    @property
+    def rent(self):
+        if not self._color_set:
+            return self._rent["Site"]
+        if self._houses != 0:
+            if self._hotel:
+                return self._rent["Hotel"]
+            else:
+                return self._rent["House"][self._houses - 1]
+        return self._rent["Color Set"]
 
 
 
