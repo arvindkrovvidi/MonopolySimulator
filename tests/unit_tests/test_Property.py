7bd1db71ae2ba58d8b2ae32c3256c1df207a9814
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-07 20:39:19 +0530

    TEST: Add tests for str and eq functions

--- tests/unit_tests/test_Property.py
+++ tests/unit_tests/test_Property.py
@@ -1,5 +1,7 @@
 import pytest
 
+from Tiles.Property import Property
+
 
 @pytest.mark.parametrize("color_set, houses, hotel, expected_rent",[
     (False, 0, False, 10),
@@ -16,3 +18,10 @@ def test_rent(st_charles_place, color_set, houses, hotel, expected_rent):
     st_charles_place._houses = houses
     st_charles_place._hotel = hotel
     assert st_charles_place.rent == expected_rent
+
+def test_str(states_avenue):
+    assert str(states_avenue) == "States Avenue"
+
+def test_eq(states_avenue, property_rent_data):
+    test_property = Property(13, "States Avenue", 140,  property_rent_data["States Avenue"]["Rent"], "Pink", 100)
+    assert test_property == states_avenue
