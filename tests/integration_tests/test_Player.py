2067da30814dfad0d51f478295bd618904083564
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-12 20:47:21 +0530

    TEST: add test for pay_rent function

--- tests/integration_tests/test_Player.py
+++ tests/integration_tests/test_Player.py
@@ -33,7 +33,7 @@ def test_pay_rent_1(arvind, arun, st_charles_place):
 
 def test_pay_rent_2(arvind, arun, st_charles_place, virginia_avenue, states_avenue, electric_company):
     """
-    test pay_rent when player does not own color set or has not built any houses or hotels.
+    test pay_rent when player owns color set
     """
     arun.move_to(electric_company.tile_no, collect_go_cash_flag=True)
     arun.move_to(st_charles_place.tile_no, collect_go_cash_flag=True)
@@ -45,3 +45,17 @@ def test_pay_rent_2(arvind, arun, st_charles_place, virginia_avenue, states_aven
     assert arvind.cash == 180
     assert arun.cash == 180
 
+def test_pay_rent_3(arvind, arun, st_charles_place, virginia_avenue, states_avenue, electric_company):
+    """
+    test pay_rent when player owns color set and has built a house
+    """
+    arun.move_to(electric_company.tile_no, collect_go_cash_flag=True)
+    arun.move_to(st_charles_place.tile_no, collect_go_cash_flag=True)
+    arun.buy_property(st_charles_place)
+    arun.buy_property(virginia_avenue)
+    arun.buy_property(states_avenue)
+    arun.build_house(st_charles_place)
+    arvind.pay_rent(arun, st_charles_place.rent)
+
+    assert arvind.cash == 150
+    assert arun.cash == 110
