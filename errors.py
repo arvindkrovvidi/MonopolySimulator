2be315c27cab08a97c13219c35993d57d8439e9e
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-12 20:32:53 +0530

    FEAT: Modify exceptions to take values when they are raised.

--- errors.py
+++ errors.py
@@ -1,5 +1,10 @@
 class PlayerBrokeError(Exception):
-    exc_message = "Player is broke!"
+    def __init__(self, player):
+        self.player = player
+        self.exc_message = self.player + " is broke!"
 
 class PropertyNotFreeError(Exception):
     exc_message = "Property is not free to buy"
+    def __init__(self, asset):
+        self.asset = asset
+        self.exc_message = self.asset + " is not free to buy"
