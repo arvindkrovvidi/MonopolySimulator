46e5caa004d521bcd521c854da30a79d22522635
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-10-29 17:41:20 +0530

    FEAT: Change game flow

--- turn_based_game.py
+++ turn_based_game.py
@@ -4,8 +4,8 @@ from Player_data import all_players_list as players
 from Tiles_data.all_tiles_data import all_tiles_list
 from config import logger
 from errors import PlayerBrokeError
-from player_turn import get_available_options_properties, run_player_option
-from utils import check_any_player_broke, print_player_summary, get_display_options
+from player_turn import play_turn
+from utils import check_any_player_broke, print_player_summary
 
 # TODO: Take all inputs for the program from a file
 total_turns = 1000
@@ -25,12 +25,7 @@ while turn <= total_turns or not check_any_player_broke(players):
         print(f'{player} landed on {current_tile}')
         logger.info(f'{player} landed on {current_tile}')
         try:
-            available_options = get_available_options_properties(current_tile, player, throw)
-            if available_options is not None:
-                option_function_dict = dict(list(enumerate(available_options)))
-                print(get_display_options(available_options))
-                user_input = int(input('Select an option from the above: '))
-                run_player_option(player, current_tile, option_function_dict, user_input)
+            play_turn(player, current_tile, throw)
             print(f"Turn: {turn}, Player: {str(player)}, cash: {player.cash} ")
             logger.info(f"Turn: {turn}, Player: {str(player)}, cash: {player.cash} ")
         except PlayerBrokeError:
