b4eadebc1e377cef6b09e383b2da216af8c2a0d8
arvindkrovvidi <arvind.akkrovvidi@gmail.com>
2023-12-03 21:01:54 +0530

    TEST: Test main

--- tests/integration_tests/test_turn_based_game.py
+++ tests/integration_tests/test_turn_based_game.py
@@ -1,5 +1,5 @@
 from turn_based_game import main
-def test_main(mocker, arvind, arun, st_charles_place):
+def test_main_1(mocker, arvind, arun, st_charles_place):
     """
     Test main function when one of the players goes broke
     """
@@ -12,3 +12,18 @@ def test_main(mocker, arvind, arun, st_charles_place):
     main(players, total_turns=10)
 
     assert players == [arun]
+
+def test_main_2(mocker, arvind, arun):
+    """
+    Test main when player lands on Chance 22, picks chance card 5, moves to B&O railroad that is already owned by another player
+    """
+    all_players_list = [arun, arvind]
+    mocker.patch.object(arun, 'throw_dice', side_effect=[10, 10, 5])
+    mocker.patch('player_turn.get_player_input', return_value=0)
+    mocker.patch.object(arvind, 'throw_dice', side_effect=[10, 10, 5])
+    mocker.patch('player_turn.get_player_input', return_value=0)
+    main(players=all_players_list, total_turns=3)
+
+    assert arvind.cash == 375
+    assert arun.cash == 225
+    assert arvind.tile_no == 25
